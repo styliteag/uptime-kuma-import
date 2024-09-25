ff1719282564b65ebd48606063a82877ed9ed32d
Wim Bonis <wb@stylite.de>
2024-09-25 13:16:49 +0200

    Update delete_group.py

--- delete_group.py
+++ delete_group.py
@@ -1,20 +1,71 @@
 #!/usr/bin/env python3
 
 import os
+import sys
 import json
+import configparser
 from dotenv import load_dotenv
 from uptime_kuma_api import UptimeKumaApi, MonitorType
 
-load_dotenv()
-
-base_url = os.getenv("BASE_URL")
-username = os.getenv("USERNAME")
-password = os.getenv("PASSWORD")
-parent = os.getenv("PARENT")
+# default values
+api_timeout=10
 
 
-api = UptimeKumaApi(base_url)
+if "-h" in sys.argv:
+  print(f"Usage: {sys.argv[0]} [-h] [-c config_file] [-v]")
+  print("  -h: show this help")
+  print("  -c config_file: use the config file (default: SCRIPTNAME.ini)")
+  print("  -v: verbose output")
+  exit(0)
+
+verbose = False
+if "-v" in sys.argv:
+  verbose = True
+
+if "-f" in sys.argv:
+  input_file_name = sys.argv[sys.argv.index("-f")+1]
+
+# Create a config_filename wich is the path iand the name of this script without the extension and with .ini
+config_file_name = os.path.splitext(sys.argv[0])[0]+".ini"
+#print(f"Config File: {config_file_name}")
+
+if "-c" in sys.argv:
+  config_file_name = sys.argv[sys.argv.index("-c")+1]
+
+if not os.path.exists(config_file_name):
+  print(f"Config File {config_file_name} not found")
+  exit(1)
+
+# Read the config file
+config = configparser.ConfigParser()
+config.read(config_file_name)
+
+# get baseurl form config
+base_url = config.get("uptimekuma", "base_url")
+username = config.get("uptimekuma", "username")
+password = config.get("uptimekuma", "password")
+parent = config.get("uptimekuma", "parent", fallback="AutoChecks")
+
+if base_url is None:
+  print("No BASE_URL given")
+  exit(1)
+if username is None:
+  print("No USERNAME given")
+  exit(1)
+if password is None:
+  print("No PASSWORD given")
+  exit(1)
+
+if verbose:
+  print(f"Base URL: {base_url}")
+  print(f"Username: {username}")
+  #print(f"Password: {password}")
+api = UptimeKumaApi(base_url ,timeout=api_timeout)
+#if verbose:
+#  print(f"API: {api}")
 api.login(username, password)
+if verbose:
+  print(f"API: {api}")
 
 monitors = api.get_monitors()
 #print(json.dumps(result, indent=2))
